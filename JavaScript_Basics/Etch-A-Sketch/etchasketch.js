a859326b5ec71227155d8ef0f9c05184a0bae883
Neil Bock <ncbock@gmail.com>
2022-12-01 14:18:00 -0500

    initial commit late

---
+++ JavaScript_Basics/Etch-A-Sketch/etchasketch.js
@@ -0,0 +1,43 @@
+let size = 16
+
+//Grab the conatiner div in the body of the HTML
+const container = document.querySelector('#container');
+
+for (let i = 0; i < size * size; i++){
+    const div = document.createElement('div');//Create a single div
+    div.classList.add("grid-item") 
+    //div.textContent = `I'm div ${i}`;
+    container.appendChild(div);
+}
+
+const gridItems = document.querySelectorAll(".grid-item");
+
+gridItems.forEach((gridItem) => {
+
+    gridItem.addEventListener('mousedown', changeColor);
+    gridItem.addEventListener('mouseover', changeColor);
+    
+});
+
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+
+function changeColor(e) {
+    if (e.type === 'mouseover' && mouseDown) {
+        e.target.style['background-color'] = 'black'
+        console.log(mouseDown);
+    }
+}
+
+const clear = document.getElementById('clear');
+
+clear.addEventListener('click', clearGrid);
+
+function clearGrid(){
+    gridItems.forEach((gridItem) => {
+        gridItem.style['background-color'] = 'white';
+    })
+}
+
