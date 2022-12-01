68a60be305e07c14f50493c9c62f640d74f989b3
Neil Bock <ncbock@gmail.com>
2022-12-01 15:36:24 -0500

    changed grid creation to function

--- JavaScript_Basics/Etch-A-Sketch/etchasketch.js
+++ JavaScript_Basics/Etch-A-Sketch/etchasketch.js
@@ -1,24 +1,24 @@
-let size = 16
+let defaultSize = 16
 let currentMode = 'black'
 
 //Grab the conatiner div in the body of the HTML
 const container = document.querySelector('#container');
 
-for (let i = 0; i < size * size; i++){
-    const div = document.createElement('div');//Create a single div
-    div.classList.add("grid-item") 
-    //div.textContent = `I'm div ${i}`;
-    container.appendChild(div);
-}
+//Create Global Grid Items Variable
 
-const gridItems = document.querySelectorAll(".grid-item");
 
-gridItems.forEach((gridItem) => {
-
+function createGrid(size){
+    for (let i = 0; i < size * size; i++){
+        const div = document.createElement('div');//Create a single div
+        div.classList.add("grid-item") 
+        container.appendChild(div);
+    }
+    const gridItems = document.querySelectorAll(".grid-item");
+    gridItems.forEach((gridItem) => {
     gridItem.addEventListener('mousedown', changeColor);
     gridItem.addEventListener('mouseover', changeColor);
-    
-});
+    });
+};
 
 let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true);
@@ -28,16 +28,34 @@ document.body.onmouseup = () => (mouseDown = false);
 function changeColor(e) {
     if (e.type === 'mouseover' && !mouseDown) return
     if (currentMode === 'random'){
-        e.target.style['background-color'] = 'blue';
+        const randomR = randomColorValue()
+        const randomG = randomColorValue()
+        const randomB = randomColorValue()
+        e.target.style['background-color'] = `rgb(${randomR},${randomG},${randomB})`;
     } else {
     e.target.style['background-color'] = 'black';
     };
 };
 
-const clear = document.getElementById('clear');
+const buttons = document.querySelectorAll('button');
 
+buttons.forEach((button) => {
+    button.addEventListener('click', setMode)
+})
+
+//Add event listener for the Clear button
+const clear = document.getElementById('clear');
 clear.addEventListener('click', clearGrid);
 
+//Add event listener for the Size button
+const sizeButton = document.getElementById('size');
+sizeButton.addEventListener('click', () => {
+    changeSize();
+    clearGrid();
+    createGrid(defaultSize);
+});
+
+//Clears the Grid
 function clearGrid(){
     gridItems.forEach((gridItem) => {
         gridItem.style['background-color'] = 'white';
@@ -45,9 +63,24 @@ function clearGrid(){
     })
 }
 
-const randomColor = document.getElementById('random');
-randomColor.addEventListener('click', setMode);
-
+//Sets the current mode
 function setMode(e){
-    currentMode = e.target.id;
+    if(e.target.id === 'random'){
+        currentMode = e.target.id;
+    }else{
+        currentMode = 'black'
+    }
+}
+
+//Creates random RGB value between 1 and 256
+function randomColorValue (){
+    return Math.floor(Math.random() * 256)
+};
+
+function changeSize(){
+    defaultSize = prompt('How big would you like the Grid?');
+}
+
+window.onload = () => {
+    createGrid(defaultSize);
 }
