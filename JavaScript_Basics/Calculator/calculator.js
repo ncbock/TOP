cf059febe2ca81caf95a74837c69b4057f7497bb
Neil Bock <ncbock@gmail.com>
2022-12-07 08:14:39 -0500

    added functionality for updating display

--- JavaScript_Basics/Calculator/calculator.js
+++ JavaScript_Basics/Calculator/calculator.js
@@ -14,12 +14,25 @@ function divide (a, b) {
     return a / b;
 }
 
+// Set inital Display to 0
+const display = document.getElementById('display');
+display.textContent = "0";
+
+
+
+
+//Get all the number buttons 
 const numbers = document.querySelectorAll('.number');
 
+//Update the display when a number is pressed
 numbers.forEach((number) => {
-    number.addEventListener('click', beenClicked);
+    number.addEventListener('click', (e) => {
+        if (display.textContent === "0"){
+            display.textContent = ""
+        }
+        if (display.textContent.includes(".") && e.target.textContent === "."){
+            return
+        }
+        display.textContent += number.textContent
+    });
 });
-
-function beenClicked() {
-    console.log("ive been clicked")
-}
